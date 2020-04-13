f5f95d33c63ae0417b7ac651a6db3aee25ef86f5
felberto <tobias.felber97@gmail.com>
2020-04-13 20:30:07 +0200

    fixed frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {QuoteService} from "./services/quote.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
   providers: [QuoteService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
